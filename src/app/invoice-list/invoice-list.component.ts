a280ebed0a02bdfe35ebfc57fa6deb800851359e
Hendrik26 <hendrik.herrdler@gmail.com>
2018-09-12 22:06:42 +0200

    introduce a constant array for the invoice-list.component....

--- src/app/invoice-list/invoice-list.component.ts
+++ src/app/invoice-list/invoice-list.component.ts
@@ -9,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class InvoiceListComponent implements OnInit {
 
   //public invoice: Invoice = {id: 1, name: 'Invoice001'};
+   invoices = [
+       {id:'1', invoiceNumber:'201800ab',  recipient: 'Mustermann GmbH', invoiceDate:'2018-01-01', timeSpan:'2017-01-01 bis 2017-12-31',
+          invoiceState:'Entwurf', wholeCost:'3000,00€', countReminders:'0'   },
+       {id:'2', invoiceNumber:'201800ab',  recipient: 'Mustermann GmbH', invoiceDate:'2018-01-01', timeSpan:'2017-01-01 bis 2017-12-31',
+           invoiceState:'Entwurf', wholeCost:'3000,00€', countReminders:'0'   }
+
+    ];
 
   constructor() { }
 
