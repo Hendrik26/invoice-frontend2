d4d77b63c5a486713226c8e1b9d8e3adcf919bf7
hendr_8nkbeie <hendrik.herdler@stud.hszg.de>
2018-09-13 22:22:16 +0200

    make the router in app-component work.

--- src/app/invoice-router.module.ts
+++ src/app/invoice-router.module.ts
@@ -4,8 +4,16 @@ import {InvoiceListComponent} from './invoice-list/invoice-list.component';
 import {InvoiceDetailComponent} from './invoice-detail/invoice-detail.component';
 import {ItemDetailComponent} from './item-detail/item-detail.component';
 
+const routes: Routes = [
+    { path: '', redirectTo: '/invoice-list', pathMatch: 'full' },
+    {path: 'invoice-list', component: InvoiceListComponent},
+    {path: 'invoice-detail', component: InvoiceDetailComponent},
+    {path: 'item-detail', component: ItemDetailComponent}
+];
+
 @NgModule({
-  exports: [RouterModule]
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [RouterModule]
 })
 export class InvoiceRouterModule {
 }
