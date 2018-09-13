03a29d3608f7b22636cace210c34a1e4480c15aa
Hendrik26 <hendrik.herdler@gmail.com>
2018-09-13 12:25:30 +0200

    recive table invoice-list completly from angular.

--- src/app/invoice-list/invoice-list.component.ts
+++ src/app/invoice-list/invoice-list.component.ts
@@ -8,14 +8,7 @@ import {Invoice002} from '../invoice002';
 })
 export class InvoiceListComponent implements OnInit {
 
-  //public invoice: Invoice = {id: 1, name: 'Invoice001'};
-  /* invoices = [
-       {id:'1', invoiceNumber:'201800ab',  recipient: 'Mustermann GmbH', invoiceDate:'2018-01-01', timeSpan:'2017-01-01 bis 2017-12-31',
-          invoiceState:'Entwurf', wholeCost:'3000,00€', countReminders:'0'   },
-       {id:'2', invoiceNumber:'201800ab',  recipient: 'Mustermann GmbH', invoiceDate:'2018-01-01', timeSpan:'2017-01-01 bis 2017-12-31',
-           invoiceState:'Entwurf', wholeCost:'3000,00€', countReminders:'0'   }
 
-    ]; */
 
   invoices: Invoice002[] = [
 
