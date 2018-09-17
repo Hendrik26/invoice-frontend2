16ee6150f618bd86b1deac35c2515dff755b765f
Hendrik26 <hendrik.herrdler@gmail.com>
2018-09-17 11:50:57 +0200

    format variable items: Item[] rightly according to square[] brackets

--- src/app/invoice-detail/invoice-detail.component.ts
+++ src/app/invoice-detail/invoice-detail.component.ts
@@ -1,30 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Item} from '../item';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
-  selector: 'app-invoice-detail',
-  templateUrl: './invoice-detail.component.html',
-  styleUrls: ['./invoice-detail.component.css']
+    selector: 'app-invoice-detail',
+    templateUrl: './invoice-detail.component.html',
+    styleUrls: ['./invoice-detail.component.css']
 })
 export class InvoiceDetailComponent implements OnInit {
 
     items: Item[] = [
 
-        {id: 1, itemDate: '2016-04-01', itemName: 'Programmieren',
-            partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'},
-        {id: 2, itemDate: '2016-04-30', itemName: 'Datenbank-Design',
-            partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'}
+        {
+            id: 1, itemDate: '2016-04-01', itemName: 'Programmieren',
+            partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
+        },
+        {
+            id: 2, itemDate: '2016-04-30', itemName: 'Datenbank-Design',
+            partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
+        }
 
-];
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  openItemDetail(methID){
+    ];
 
 
-  }
+    constructor(
+        private route: ActivatedRoute,
+        private location: Location
+    ) {
+    }
+
+    ngOnInit() {
+        this.getInvoice();
+    }
+
+    getInvoice(): void {
+        const id = +this.route.snapshot.paramMap.get('id');
+    }
+
+    openItemDetail(methID) {
+
+
+    }
 
 }
