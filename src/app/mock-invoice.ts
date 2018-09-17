704021d9e45b17b70c31d665d2178c8e795a44e8
Hendrik26 <hendrik.herrdler@gmail.com>
2018-09-17 20:49:25 +0200

    introduces a file mock-invoice to prepare introducing a Service.

---
+++ src/app/mock-invoice.ts
@@ -0,0 +1,50 @@
+import { Invoice } from './invoice';
+
+export const INVOICE: Invoice[] = [
+
+      {
+        id: 1, invoiceDate: new Date('04. February 2016'), invoiceNumber: '2018abcd', recipient: 'DusselGmbH',
+        invoiceState: 'Entwurf', wholeCost: 1111.11, countReminders: 0, timeSpan: '2017-01-01 bis 2017-12-31', currency: '€',
+          items: [ // item-information added
+
+              {
+                  id: 1, itemDate: '2016-04-01', itemName: 'Programmieren',
+                  partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
+              },
+              {
+                  id: 2, itemDate: '2016-04-30', itemName: 'Datenbank-Design',
+                  partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
+              }
+
+          ]},
+      {
+        id: 2, invoiceDate: new Date('05. February 2016'), invoiceNumber: '2018efgh', recipient: 'PlatinHuhn',
+        invoiceState: 'Entwurf', wholeCost: 2222.221, countReminders: 0, timeSpan: '2016-01-01 bis 2016-12-31', currency: '€',
+           items: [ // item-information added
+
+                        {
+                            id: 1, itemDate: '2016-04-01', itemName: 'Programmieren',
+                            partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
+                        },
+                        {
+                            id: 2, itemDate: '2016-04-30', itemName: 'Datenbank-Design',
+                            partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
+                        }
+
+                    ]},
+      {
+        id: 3, invoiceDate: new Date('06. February 2016'), invoiceNumber: '2018ijkl', recipient: 'Schweizer Käse&Socken GmbH',
+        invoiceState: 'Entwurf', wholeCost: 333.3321, countReminders: 0, timeSpan: '2015-01-01 bis 2015-12-31', currency: '€',
+           items: [ // item-information added
+
+                        {
+                            id: 1, itemDate: '2016-04-01', itemName: 'Programmieren',
+                            partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
+                        },
+                        {
+                            id: 2, itemDate: '2016-04-30', itemName: 'Datenbank-Design',
+                            partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
+                        }
+
+                    ]},
+    ];
