9d8bf5944b69b1d5b80b169e679854b75cab26af
Hendrik26 <hendrik.herrdler@gmail.com>
2018-09-17 21:34:13 +0200

    publishes Service "invoice.service" to invoice-detail.component.

--- src/app/invoice-detail/invoice-detail.component.ts
+++ src/app/invoice-detail/invoice-detail.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Item} from '../item';
+import {InvoiceService} from '../invoice.service';
 
 // new imports added
 import {ActivatedRoute} from '@angular/router';
@@ -13,42 +14,44 @@ import {Invoice} from '../invoice';
     styleUrls: ['./invoice-detail.component.css']
 })
 export class InvoiceDetailComponent implements OnInit {
-    invoices: Invoice[];
-    /* items: Item[] = [
+    /* invoices: Invoice[];
+    * items: Item[] = [
 
-        {
-            id: 1, itemDate: '2016-04-01', itemName: 'Programmieren',
-            partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
-        },
-        {
-            id: 2, itemDate: '2016-04-30', itemName: 'Datenbank-Design',
-            partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
-        }
+         {
+             id: 1, itemDate: '2016-04-01', itemName: 'Programmieren',
+             partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
+         },
+         {
+             id: 2, itemDate: '2016-04-30', itemName: 'Datenbank-Design',
+             partialCost: 60.00, count: 5, wholeCost: 300, hourPayment: false, currency: '€'
+         }
 
-    ]; */
+     ]; */
 
     invoiceId: number;
 
+    invoices: Invoice[];
 
     constructor(
         private route: ActivatedRoute,
         private location: Location,
-        private invoiceListComponent: InvoiceListComponent // import of invoice-list
+        private invoiceService: InvoiceService
     ) {
     }
 
     ngOnInit() {
         this.receiveInvoiceId();
-        this.receiveInvoices();
+        this.getInvoices();
+    }
+
+    getInvoices(): void {
+        this.invoices = this.invoiceService.getInvoices();
     }
 
     receiveInvoiceId(): void {
         this.invoiceId = +this.route.snapshot.paramMap.get('invoiceId');  // get invoiceID from URL
     }
 
-    receiveInvoices(): void {
-        this.invoices = this.invoiceListComponent.invoices; // Is this OK???
-    }
 
     openItemDetail(methID) {
 
