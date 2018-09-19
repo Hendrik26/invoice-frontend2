1280868b9a374e51ea04045dc59ea3d2de21d7fd
Hendrik26 <hendrik.herrdler@gmail.com>
2018-09-19 13:39:24 +0200

    prepare introducing item-service

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Item} from '../item';
+import {Invoice} from '../invoice';
 
 @Component({
   selector: 'app-item-detail',
@@ -10,6 +11,14 @@ export class ItemDetailComponent implements OnInit {
 
   currentItem: Item = {hourPayment: true, itemDate: "2016-04-01", count: 1, partialCost: 30,
     itemName: 'C#-Entwicklung', currency: '€', wholeCost: 10, id: 1};
+
+    invoiceId: number;
+    itemId: number;
+    item: Item;
+    nettoSum: number;
+    percentageString: string;
+    salesTax: number;
+    bruttoSum: number;
 
   constructor() { }
 
