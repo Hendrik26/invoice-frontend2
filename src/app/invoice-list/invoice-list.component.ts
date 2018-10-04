39ae63de62ca740a0811efe66ea31eb9a356ec0a
Hendrik26 <hendrik.herrdler@gmail.com>
2018-10-05 00:49:05 +0200

    Error with Date-Objects: Method Date.prototype.getTime() does not work for filter-Dates.

--- src/app/invoice-list/invoice-list.component.ts
+++ src/app/invoice-list/invoice-list.component.ts
@@ -3,43 +3,69 @@ import {Invoice} from '../invoice';
 import {InvoiceService} from '../invoice.service';
 
 @Component({
-  selector: 'app-invoice-list',
-  templateUrl: './invoice-list.component.html',
-  styleUrls: ['./invoice-list.component.css']
+    selector: 'app-invoice-list',
+    templateUrl: './invoice-list.component.html',
+    styleUrls: ['./invoice-list.component.css']
 })
 export class InvoiceListComponent implements OnInit {
 
 
-   // invoicesNew: Invoice[] = [{...this.invoiceService.standardInvoice}]; // clones this.standardInvoice
+    // invoicesNew: Invoice[] = [{...this.invoiceService.standardInvoice}]; // clones this.standardInvoice
 
 
-   // invoicesNew: Invoice[] = [new Invoice('In001', this.)]; // clones this.standardInvoice
+    // invoicesNew: Invoice[] = [new Invoice('In001', this.)]; // clones this.standardInvoice
 
-  invoices: Invoice[];
-  filterStartDate: Date;
-  filterEndDate: Date;
+    invoices: Invoice[];
+    filterStartDate: Date;
+    filterEndDate: Date;
 
-  constructor(private invoiceService: InvoiceService) {
-  }
+    constructor(private invoiceService: InvoiceService) {
+    }
 
-  ngOnInit() {
-    this.receiveInvoices();
-    /* if (this.invoices.length < 1) {
-      this.invoices.push(new Invoice('Inn1', this.invoiceService.standardInvoice))
-    } */
-  }
+    ngOnInit() {
+        this.receiveInvoices();
+        /* if (this.invoices.length < 1) {
+          this.invoices.push(new Invoice('Inn1', this.invoiceService.standardInvoice))
+        } */
+    }
 
-  filter(invoices: Invoice[]): Invoice[]{
-      // TODO filter
-    return invoices
-      .filter(invoice => invoice.invoiceDate.getTime() >= this.filterStartDate.getTime())
-      .filter(invoice => invoice.invoiceDate.getTime() <= this.filterEndDate.getTime())
-      ;
-  }
+    // getter
+    private getGreatPastDate(): Date {
+        return new Date('1871-01-18');
+    }
 
-  receiveInvoices(): void {
-    this.invoiceService.getInvoices()
-      .subscribe(invoices => this.invoices = invoices);
-  }
+    // other methods
+    changeFilterStartDate(e: Date) {
+        console.log('Methode changeFilterStartDate(...) aufgerufen mit: ' + e.toString());
+        this.filterStartDate = e;
+       // this.filterInvoice(this.invoices);
+    }
+
+    public dateGreaterEqualThen(date1: Date, date2: Date): boolean {
+        console.log('Start method dateGreaterEqualThen(...){...}');
+        if (date1 == undefined) { return true; }
+        console.log('First DateComparison!');
+        if (date2 == undefined) { return true; }
+        console.log('Second DateComparison!');
+        let ret: boolean = (date1.valueOf() >= date2.valueOf());
+        console.log('Third DateComparison!, ret=' + ret + ' Finish method');
+        return ret;
+    }
+
+    filterInvoice(invoices: Invoice[]): Invoice[] {
+        console.log('Start Method filterInvoice');
+        // TODO filter
+        return invoices
+        .filter(invoice => this.dateGreaterEqualThen(invoice.invoiceDate, this.filterStartDate))
+           // .filter(invoice => invoice.invoiceDate.getTime() >= this.getGreatPastDate().getTime())
+            // .filter(invoice => invoice.invoiceDate.getTime() <= this.filterEndDate.getTime())
+            ;
+        // console.log('Finish Method Filter');
+    }
+
+    receiveInvoices(): void {
+        this.invoiceService.getInvoices()
+            .subscribe(invoices => this.invoices = invoices);
+    }
 
 }
