055ba0c0a279b0ff60331c94b252d8cfb76620a9
Hendrik26 <hendrik.herrdler@gmail.com>
2018-10-07 13:53:23 +0200

    insert select-field for companyName to invoice-list.component.html (filter) with logic (to calculate list of companyNames), but calculation does not work.

--- src/app/invoice-list/invoice-list.component.ts
+++ src/app/invoice-list/invoice-list.component.ts
@@ -19,27 +19,30 @@ export class InvoiceListComponent implements OnInit {
     filterStartDate: Date;
     filterEndDate: Date;
     invoiceFilterState: 'none';
+    companySelectOptions: object[];
+    invoiceFilterCompany: string;
 
     constructor(private invoiceService: InvoiceService) {
     }
 
     ngOnInit() {
         this.receiveInvoices();
-        /* if (this.invoices.length < 1) {
-          this.invoices.push(new Invoice('Inn1', this.invoiceService.standardInvoice))
-        } */
+        this.companySelectOptions = this.calculateCompanySelectOptions(this.invoices);
     }
 
-    // getter
+    //region getter
     private getGreatPastDate(): Date {
         return new Date('1871-01-18');
     }
 
-    // other methods
+    //endregion
+
+
+    //region other methods
     changeFilterStartDate(e: string) {
         console.log('Methode changeFilterStartDate(...) aufgerufen mit: ', e);
         this.filterStartDate = e ? new Date(e) : null;
-       // this.filterInvoice(this.invoices);
+        // this.filterInvoice(this.invoices);
     }
 
     changeFilterEndDate(e: string) {
@@ -49,9 +52,13 @@ export class InvoiceListComponent implements OnInit {
 
     public dateGreaterEqualThen(date1: Date, date2: Date): boolean {
         console.log('Start method dateGreaterEqualThen(...){...}');
-        if (!date1) { return true; }
+        if (!date1) {
+            return true;
+        }
         console.log('First DateComparison!');
-        if (!date2) { return true; }
+        if (!date2) {
+            return true;
+        }
         console.log('Second DateComparison!');
         const ret: boolean = (date1.getTime() >= date2.getTime());
         console.log('Third DateComparison!, ret=' + ret + ' Finish method');
@@ -59,27 +66,45 @@ export class InvoiceListComponent implements OnInit {
     }
 
     private checkInvoiceState(invoice: Invoice, filterState: string): boolean {
-      if (filterState.trim() == '') return true;
-      if (filterState.trim().toLowerCase() == 'none') return true;
-      if (filterState.trim().toLocaleLowerCase() == 'kein') return true;
-      if (filterState.trim() == undefined) return true;
-      if (filterState.trim() == null) return true;
-      if (filterState.trim() == invoice.invoiceState) {
-        return true;
-      } else {
-        return false;
-      }
+        if (filterState.trim() == '') return true;
+        if (filterState.trim().toLowerCase() == 'none') return true;
+        if (filterState.trim().toLocaleLowerCase() == 'kein') return true;
+        if (filterState.trim() == undefined) return true;
+        if (filterState.trim() == null) return true;
+        if (filterState.trim() == invoice.invoiceState) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private checkInvoiveCompany(invoice: Invoice, companyNames: string[]): boolean {
+        let ret = false;
+        companyNames.forEach(function (value) {
+            if (value.trim().toLowerCase() == invoice.companyName().trim().toLowerCase()) {
+                ret = true;
+            }
+        });
+        return ret;
+    }
+
+    calculateCompanySelectOptions(invoices: Invoice[]): object[] {
+        const retList: object[] = [];
+        invoices.forEach(function (fktInvoice) {
+            retList.push({value: fktInvoice.companyName(), name: fktInvoice.companyName()});
+        });
+        return retList;
     }
 
     filterInvoice(invoices: Invoice[]): Invoice[] {
         console.log('Start Method filterInvoice' +
-          '');
+            '');
         // TODO filter
         return invoices
-         .filter(invoice => this.dateGreaterEqualThen(invoice.invoiceDate, this.filterStartDate))
-           // .filter(invoice => invoice.invoiceDate.getTime() >= this.getGreatPastDate().getTime())
-             .filter(invoice => this.dateGreaterEqualThen(this.filterEndDate, invoice.invoiceDate))
-          .filter(invoice => this.checkInvoiceState(invoice, this.invoiceFilterState))
+            .filter(invoice => this.dateGreaterEqualThen(invoice.invoiceDate, this.filterStartDate))
+            // .filter(invoice => invoice.invoiceDate.getTime() >= this.getGreatPastDate().getTime())
+            .filter(invoice => this.dateGreaterEqualThen(this.filterEndDate, invoice.invoiceDate))
+            .filter(invoice => this.checkInvoiceState(invoice, this.invoiceFilterState))
             ;
         // console.log('Finish Method Filter');
     }
@@ -88,5 +113,7 @@ export class InvoiceListComponent implements OnInit {
         this.invoiceService.getInvoices()
             .subscribe(invoices => this.invoices = invoices);
     }
+
+    //endregion
 
 }
--- src/app/invoice.ts
+++ src/app/invoice.ts
@@ -3,107 +3,116 @@ import {InvoiceType} from './invoice-type';
 import {ItemType} from './item-type';
 
 export class Invoice implements InvoiceType {
-  // //////////////////////
-  // static properties
-  private static emptyData: InvoiceType = {
-    invoiceDate: new Date(),
-    invoiceDueDate: new Date(),
-    invoiceNumber: '2018axbycz',
-    recipient: '',
-    invoiceState: 'Entwurf',
-    wholeCost: 0,
-    countReminders: 0,
-    timeSpan: 'unknown',
-    salesTaxPercentage: 19
-  };
+    // //////////////////////
+    // static properties
+    private static emptyData: InvoiceType = {
+        invoiceDate: new Date(),
+        invoiceDueDate: new Date(),
+        invoiceNumber: '2018axbycz',
+        recipient: '',
+        invoiceState: 'Entwurf',
+        wholeCost: 0,
+        countReminders: 0,
+        timeSpan: 'unknown',
+        salesTaxPercentage: 19
+    };
 
-  // IDs
-  private id: string; // <th>Ändern</th>
-
-  // other properties
-  countReminders: number; // <th>Anzahl der Mahnungen</th>
-  currency = '€';
-  invoiceDate: Date; // <th>Rechnungsdatum</th>
-  invoiceDueDate: Date; // Faelligkeitsdatum
-  invoiceNumber: string; // <th>RechnungsNr</th>
-  invoiceIntendedUse; // Verwendungszweck
-  invoiceState: string; // <th>Status (Entwurf, bezahlt, ...)</th>
-  items: Item[];
-  recipient: string; // <th>Empfänger</th>
-  salesTaxPercentage: number;
-  timeSpan: string; // <th>Rechnungzeitraum</th>
-  wholeCost: number; // <th>Gesamtpreis</th>
-
-  constructor(id: string, data: InvoiceType) {
     // IDs
-    this.id = id; // New Commit after problems with merging
+    private id: string; // <th>Ändern</th>
 
     // other properties
-    this.invoiceDate = data.invoiceDate;
-    this.invoiceDueDate = data.invoiceDueDate;
-    this.invoiceNumber = data.invoiceNumber;
-    this.recipient = data.recipient;
-    this.invoiceState = data.invoiceState;
-    this.wholeCost = data.wholeCost;
-    this.countReminders = data.countReminders;
-    this.timeSpan = data.timeSpan;
-    this.currency = data.currency || '€';
-    this.salesTaxPercentage = data.salesTaxPercentage;
-    this.items = [];
-  }
+    countReminders: number; // <th>Anzahl der Mahnungen</th>
+    currency = '€';
+    invoiceDate: Date; // <th>Rechnungsdatum</th>
+    invoiceDueDate: Date; // Faelligkeitsdatum
+    invoiceNumber: string; // <th>RechnungsNr</th>
+    invoiceIntendedUse; // Verwendungszweck
+    invoiceState: string; // <th>Status (Entwurf, bezahlt, ...)</th>
+    items: Item[];
+    recipient: string; // <th>Empfänger</th>
+    salesTaxPercentage: number;
+    timeSpan: string; // <th>Rechnungzeitraum</th>
+    wholeCost: number; // <th>Gesamtpreis</th>
 
-  // static methods
-  public static createNewInvoiceId(): string {
-    const methDate: Date = new Date();
-    return 'Inv' + methDate.getTime();
-  }
+    constructor(id: string, data: InvoiceType) {
+        // IDs
+        this.id = id; // New Commit after problems with merging
 
-  public static createNewInvoice(): Invoice { // factory pattern, prime example
-    let methInvoice: Invoice;
-    methInvoice = new Invoice(this.createNewInvoiceId(), this.emptyData);
-    return methInvoice;
-  }
-
-  public static firstLine(inString: string): string {
-    const lines = inString.split('\n');
-    return lines[0];
-  }
-
-  public companyName(): string {
-    return Invoice.firstLine(this.recipient);
-  }
-
-
-  // getter
-  public getID(): string {
-    return this.id;
-  }
-
-  private getMaxItemId(): number {
-    console.log('invoice.getMaxItemId');
-    if (this.items === undefined) {
-      return -1;
-    } else {
-      return this.items.reduce<number>((a: number, x: Item) => {
-        return Math.max(a, x.getItemId());
-      }, -1); // lambda-expression
+        // other properties
+        this.invoiceDate = data.invoiceDate;
+        this.invoiceDueDate = data.invoiceDueDate;
+        this.invoiceNumber = data.invoiceNumber;
+        this.recipient = data.recipient;
+        this.invoiceState = data.invoiceState;
+        this.wholeCost = data.wholeCost;
+        this.countReminders = data.countReminders;
+        this.timeSpan = data.timeSpan;
+        this.currency = data.currency || '€';
+        this.salesTaxPercentage = data.salesTaxPercentage;
+        this.items = [];
     }
-  }
 
-  // setter
+    // static methods
+    public static createNewInvoiceId(): string {
+        const methDate: Date = new Date();
+        return 'Inv' + methDate.getTime();
+    }
 
-  // otherr methods
-  public addNewItem(itemType: ItemType): number {
-    // TODO add new Item
-    console.log('invoice.addNewItem');
-    const createdItem = new Item(this, itemType);
-    this.items.push(createdItem);
-    // return new Item(this, item);
-    return createdItem.getItemId();
-  }
+    public static createNewInvoice(): Invoice { // factory pattern, prime example
+        let methInvoice: Invoice;
+        methInvoice = new Invoice(this.createNewInvoiceId(), this.emptyData);
+        return methInvoice;
+    }
 
-  public computeNextItemId(): number {
-    return this.getMaxItemId() + 1;
-  }
+    public static firstLine(inString: string): string {
+        const lines = inString.split('\n');
+        return lines[0];
+    }
+
+    public static companyNames(invoices: Invoice[]): string[] {
+        let companyNameList: string[] = [];
+        invoices.forEach(function (value) {
+            companyNameList.push(value.companyName());
+        });
+        return companyNameList;
+    }
+
+
+    public companyName(): string {
+        return Invoice.firstLine(this.recipient);
+    }
+
+
+    // getter
+    public getID(): string {
+        return this.id;
+    }
+
+    private getMaxItemId(): number {
+        console.log('invoice.getMaxItemId');
+        if (this.items === undefined) {
+            return -1;
+        } else {
+            return this.items.reduce<number>((a: number, x: Item) => {
+                return Math.max(a, x.getItemId());
+            }, -1); // lambda-expression
+        }
+    }
+
+    // setter
+
+    // otherr methods
+    public addNewItem(itemType: ItemType): number {
+        // TODO add new Item
+        console.log('invoice.addNewItem');
+        const createdItem = new Item(this, itemType);
+        this.items.push(createdItem);
+        // return new Item(this, item);
+        return createdItem.getItemId();
+    }
+
+    public computeNextItemId(): number {
+        return this.getMaxItemId() + 1;
+    }
 
 }
